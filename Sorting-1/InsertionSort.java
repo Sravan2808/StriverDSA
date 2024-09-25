b609caaef0b42ac7e2e215fe272fcc0e572a10a7
Sravan Surya <vsravansurya@gmail.com>
2024-09-25 22:14:51 +0530

    InsertionSort

--- Sorting-1/InsertionSort.java
+++ Sorting-1/InsertionSort.java
@@ -1,18 +1,16 @@
-package Sorting-1;
-
-public class InsertionSort {
-    public void insertionSort(int arr[], int n)
-  {
-      //code here
-      for(int i=0;i<=arr.length-1;i++){
-          int j=i;
-          while(j>0 && arr[j-1]>arr[j]){
-              int temp = arr[j];
-              arr[j] = arr[j-1];
-              arr[j-1] = temp;
-              j--;
-          }
-      }
-  }
+public class insertionSort {
+    void selectionSort(int arr[], int n)
+	{
+	    //code here
+	     for(int i=0;i<=arr.length-2;i++){
+            int mini=i;
+            for(int j=i;j<arr.length;j++){
+                if(arr[j]<arr[mini]) mini=j;
+            }
+            int temp =arr[i];
+            arr[i]=arr[mini];
+            arr[mini]=temp;
+        }
+	}
     
 }
