de51ba18b6a7241707844daf88d0d03bc49493b0
Sravan Surya <vsravansurya@gmail.com>
2024-09-25 21:49:20 +0530

    InsertionSort

--- Sorting-1/InsertionSort.java
+++ Sorting-1/InsertionSort.java
@@ -1,6 +1,7 @@
 public void insertionSort(int arr[], int n)
 {
     //code here
+    // Time Complexity:O(n^2)
     for(int i=0;i<=arr.length-1;i++){
         int j=i;
         while(j>0 && arr[j-1]>arr[j]){
