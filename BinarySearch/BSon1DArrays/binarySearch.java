825a2cc8b064c7a5e0a0622944fe96edafb8642e
Sravan Surya <vsravansurya@gmail.com>
2024-09-25 23:23:33 +0530

    binarySearch

---
+++ BinarySearch/BSon1DArrays/binarySearch.java
@@ -0,0 +1,13 @@
+public int search(int[] nums, int target) {
+    // Iterative Approach
+    int n =nums.length;
+    int low =0;
+    int high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]==target) return mid;
+        else if(target>nums[mid]) low=mid+1;
+        else high=mid-1;
+    }
+    return -1;
+}
