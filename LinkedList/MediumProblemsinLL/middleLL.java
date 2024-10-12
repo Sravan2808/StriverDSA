29839a4c8e52c52214b701d3ca6fde2865abf6d6
Sravan Surya <vsravansurya@gmail.com>
2024-10-12 22:51:10 +0530

    Middle of a LinkedList [TortoiseHare Method]

---
+++ LinkedList/MediumProblemsinLL/middleLL.java
@@ -0,0 +1,45 @@
+package LinkedList.MediumProblemsinLL;
+
+
+  
+ public class ListNode {
+    int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+
+public class middleLL {
+    public ListNode middleNode(ListNode head) {
+        // Brute Force Approach TC:O(N+N/2) SC:O(1)
+        // if(head==null || head.next == null) return head;
+        // ListNode temp = head;
+        // int cnt = 0;
+        // while(temp!=null){
+        //     cnt++;
+        //     temp=temp.next;
+        // }
+        // int midNode = (cnt/2)+1;
+        // temp=head;
+        // while(temp!=null){
+        //     midNode--;
+        //     if(midNode == 0)
+        //     break;
+        //     temp=temp.next;
+        // }
+        // return temp;
+
+        // Optimal Approach
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+
+        while(fastPtr !=null && fastPtr.next != null){
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+        return slowPtr;
+    }
+    
+}
