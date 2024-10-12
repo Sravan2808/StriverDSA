0783974e3bfe27e649e938cade2f443b2dd49bb0
Sravan Surya <vsravansurya@gmail.com>
2024-10-13 00:17:46 +0530

    Reverse a LL [Recursive]

---
+++ LinkedList/MediumProblemsinLL/recursiveReverseLL.java
@@ -0,0 +1,21 @@
+  /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class recursiveReverseLL {
+    public ListNode reverseList(ListNode head) {
+  // Recursive Approach TC:O(n) SC:O(n)
+    if(head == null || head.next==null) return head;
+    ListNode newHead = reverseList(head.next);
+    ListNode front = head.next;
+    front.next = head;
+    head.next = null;
+    return newHead;
+    }
+}
