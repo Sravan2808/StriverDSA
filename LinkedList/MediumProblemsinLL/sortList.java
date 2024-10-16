1e73d293cd769febd051615c4901f3afe50a1c59
Sravan Surya <vsravansurya@gmail.com>
2024-10-16 18:07:23 +0530

    .\countNodeinLoop.java

---
+++ LinkedList/MediumProblemsinLL/sortList.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        List<Integer> arr = new ArrayList<Integer>();
+        ListNode temp = head;
+        while(temp!=null){
+            arr.add(temp.val);
+            temp = temp.next;
+        }
+        Collections.sort(arr);
+        int i=0;
+        temp = head;
+        while(temp!=null){
+            temp.val=arr.get(i);
+            i++;
+            temp=temp.next;
+        }
+        return head;
+        
+    }
+}
