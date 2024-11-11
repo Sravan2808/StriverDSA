6a855f8b664b9c4b0b882174db1f38f8a8a4af41
Sravan Surya <vsravansurya@gmail.com>
2024-11-11 17:23:28 +0530

    Count number of bits to be flipped to convert A to B

---
+++ BitManipulation/InterviewProblems/minBitFlips.java
@@ -0,0 +1,15 @@
+package BitManipulation.InterviewProblems;
+
+public class minBitFlips {
+    public int minBitFlips(int start, int goal) {
+        int ans = start^goal;
+        int cnt = 0;
+        for(int i=0;i<31;i++){
+            if((ans & (1<<i)) != 0)
+            cnt++;
+        }
+        return cnt;
+        
+    }
+    
+}
