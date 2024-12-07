987797ff181ec86863eadea2e3861d0c5f9a25c6
Sravan Surya <vsravansurya@gmail.com>
2024-12-07 12:43:17 +0530

    stringintchar

---
+++ Strings/stringintchar.java
@@ -0,0 +1,14 @@
+package Strings;
+import java.util.*;;
+
+public class stringintchar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String str = "abc";
+        // str = str+"xyz";
+        // str+=10;
+        System.out.println("abc"+10+20);
+        System.out.println(10+20+"abc");
+    }
+    
+}
