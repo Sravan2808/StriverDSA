23e3ccd366108f27d1f07494fcb1e5e69d30cc47
Sravan Surya <vsravansurya@gmail.com>
2024-12-07 12:41:36 +0530

    Interning

---
+++ Strings/interning.java
@@ -0,0 +1,11 @@
+package Strings;
+import java.util.*;
+
+public class interning {
+    public static void main(String[] args) {
+        String s = "Hello";
+        String x = "Hello";
+        x = "Mello";
+        System.out.println(x );
+    }
+}
