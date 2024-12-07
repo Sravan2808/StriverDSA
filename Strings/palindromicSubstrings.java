8ba1a211111cd3ff06b5f6fea2be2c6d255e39b5
Sravan Surya <vsravansurya@gmail.com>
2024-12-07 14:20:36 +0530

    palindromic subStrings & it's count

---
+++ Strings/palindromicSubstrings.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class palindromicSubstrings {
+    public static boolean isPalindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        String str = "abcba";
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<=str.length();j++){
+                if(isPalindrome(str.substring(i, j)) == true){
+                    System.out.print(str.substring(i,j)+" ");
+                    count++;
+                }
+            }
+        }
+        System.out.println("\n The number of palindromic substrings are : "+count);
+    }
+}
