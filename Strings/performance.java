dfe11ff179658db507bcf665fad23284eed31abd
Sravan Surya <vsravansurya@gmail.com>
2024-12-07 12:44:09 +0530

    Performance of string(Immutable)

---
+++ Strings/performance.java
@@ -0,0 +1,12 @@
+package Strings;
+import java.util.*;
+public class performance {
+    public static void main(String[] args) {
+        String str = "";
+        for(int i=1;i<=10;i++){
+            str=str+i;
+        }
+        System.out.println(str);
+    }
+    
+}
