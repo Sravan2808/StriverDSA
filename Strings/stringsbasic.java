ab716212448cd42509512233a40077d3035cd3dc
Sravan Surya <vsravansurya@gmail.com>
2024-12-07 12:42:29 +0530

    StringInBuilt in java

---
+++ Strings/stringsbasic.java
@@ -0,0 +1,30 @@
+package Strings;
+import java.util.*;
+
+public class stringsbasic {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // String s = sc.nextLine();
+        // System.out.println(s);
+        // String str = "Hello";
+        // String gtr = "Hello";
+        // System.out.println(str.length());
+        // System.out.println(str.charAt(0));
+        // System.out.println(str.indexOf('e'));
+        // System.out.println(str.compareTo(gtr));
+
+        // String str = "Physics Wallah";
+        // System.out.println(str.contains("all"));
+        // System.out.println(str.startsWith("Phy"));
+        // System.out.println(str.endsWith("ah"));
+        // System.out.println(str.toLowerCase());
+        // System.out.println(str.toUpperCase());
+
+        String s1 = "abc";
+        String s2 = "def";
+
+        System.out.println(s1.concat(s2));
+        
+
+    }
+}
