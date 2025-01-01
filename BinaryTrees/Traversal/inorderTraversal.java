84fd7a84afb6f5bae2c4bcbcb3ffdb1108beb540
Sravan Surya <vsravansurya@gmail.com>
2025-01-01 14:05:50 +0530

    Inorder Traversal of Binary Tree

---
+++ BinaryTrees/Traversal/inorderTraversal.java
@@ -0,0 +1,32 @@
+package BinaryTrees.Traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class inorderTraversal {
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null) return result;
+
+        inorderTraversal(root.left);
+        result.add(root.val);
+        inorderTraversal(root.right);
+
+        return result;        
+    }
+}
