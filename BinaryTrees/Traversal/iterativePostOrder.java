9d8691c11e1a4798a3367632426b29960ff8637b
Sravan Surya <vsravansurya@gmail.com>
2025-01-03 18:59:54 +0530

    Post-order Traversal of Binary Tree using 2 stack

---
+++ BinaryTrees/Traversal/iterativePostOrder.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class iterativePostOrder {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        ArrayList<Integer> postOrder = new ArrayList<>();
+
+        if(root == null) return postOrder;
+
+        st1.push(root);
+        while(!st1.isEmpty()){
+             root = st1.pop();
+             st2.push(root);
+
+             if(root.left!=null) st1.push(root.left);
+             if(root.right!=null) st1.push(root.right);
+        }
+        while(!st2.isEmpty())
+        postOrder.add(st2.pop().val);
+        return postOrder;
+    }
+
+}
